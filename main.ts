f1ae6534142760f3b7fb4b89147e7c30e10530a5
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 14:28:33 +0200

    massive code changes

--- main.ts
+++ main.ts
@@ -6,24 +6,64 @@ enum ServoChannelArm {
     S4 = REG_ADD_SERVO_4
 };
 
-//% color = #f705e3
 namespace roboArm
 {
     //% block="set servo %servo for base position to %position degrees"
-    //% position.min=-90 position.max=90
+    //% position.min=0 position.max=180
     export function turnBase(servo: ServoChannelArm, position: number):void
     {
-        position = rekabit.limit(position, -90, 90);
+        position = rekabit.limit(position, 0, 180);
         let pulseWidth = position * 20 / 18 + 50
         rekabit.i2cWrite(servo, pulseWidth);
     }
 
-    //% block="set all servos to home"
-    export function setToHomePosition():void{
-        let pulseWidth =  50
-        rekabit.i2cWrite(ServoChannelArm.S1, pulseWidth);
-        rekabit.i2cWrite(ServoChannelArm.S2, pulseWidth);
-        rekabit.i2cWrite(ServoChannelArm.S3, pulseWidth);
-        rekabit.i2cWrite(ServoChannelArm.S4, pulseWidth);
+    //% block="set servo %servo for arm1 position to %position degrees"
+    //% position.min=0 position.max=180
+    export function arm1(servo: ServoChannelArm, position: number): void {
+        position = rekabit.limit(position, 0, 180);
+        let pulseWidth = position * 20 / 18 + 50
+        rekabit.i2cWrite(servo, pulseWidth);
+    }
+
+    //% block="set servo %servo for arm2 position to %position degrees"
+    //% position.min=0 position.max=180
+    export function arm2(servo: ServoChannelArm, position: number): void {
+        position = rekabit.limit(position, 0, 180);
+        let pulseWidth = position * 20 / 18 + 50
+        rekabit.i2cWrite(servo, pulseWidth);
+    }
+
+    //% block="set servo %servo for gripper position to %position degrees"
+    //% position.min=0 position.max=90
+    export function gripper(servo: ServoChannelArm, position: number): void {
+        position = rekabit.limit(position, 0, 90);
+        let pulseWidth = position * 20 / 18 + 50
+        rekabit.i2cWrite(servo, pulseWidth);
+    }
+
+
+    //% block="increment servo: %servo by  %position degrees within %min and %max"
+    export function incrementServoByDegrees(servo: ServoChannelArm, position: number, min:number,max:number): void {
+        position = rekabit.limit(position, 0, 180)
+        position = getCurrentPositionOfServo(servo)+position
+        if(position>max)
+        {
+            position = max;
+        }
+
+        if(position<min)
+        {
+            position = min;
+        }
+        let pulseWidth = position * 20 / 18 + 50
+        rekabit.i2cWrite(servo, pulseWidth);
+    }
+
+
+    //% block="get servo position for %servo"
+    export function getCurrentPositionOfServo(servo:ServoChannelArm):number
+    {
+        let result = (rekabit.i2cRead(servo) - 50) * 18 / 20
+        return (rekabit.i2cRead(servo) -50)*18/20;
     }
 }
